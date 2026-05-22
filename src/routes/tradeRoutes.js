595d1f61541fe06e44ea58f9da8687df5285f759
Colares <colliebotv1@gmail.com>
2026-05-22 13:49:08 -0300

    Add createTrade endpoint to routes

--- src/routes/tradeRoutes.js
+++ src/routes/tradeRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const tradeController = require('../controllers/tradeController');
 
 router.get('/time', tradeController.getTime);
+router.post('/create', tradeController.createTrade);
 
 module.exports = router;
